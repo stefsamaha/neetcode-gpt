193b4784157fb8d708495384539729ad842e94b4
Stefanie Samaha <115493124+stefsamaha@users.noreply.github.com>
2026-09-18 18:56:16 +0200

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -4,26 +4,35 @@ from typing import Tuple
 
 
 class Solution:
-    def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
-        # x: 1D input array
-        # w: 1D weight array
-        # b: scalar bias
-        # y_true: true target value
-        #
-        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
-        # Loss: L = 0.5 * (y_hat - y_true)^2
-        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
-        z = np.dot(x, w) + b 
+    def backward(
+        self,
+        x: NDArray[np.float64],
+        w: NDArray[np.float64],
+        b: float,
+        y_true: float
+    ) -> Tuple[NDArray[np.float64], float]:
+
+        # ----- Forward pass -----
+        z = np.dot(x, w) + b
         y_hat = 1.0 / (1.0 + np.exp(-z))
 
-        error = y_hat - y_true 
-        sigmoid_deriv = y_hat * (1.0 - y_hat)
-        delta = error * sigmoid_deriv 
+        # ----- Backward pass -----
 
-        dL_dw = np.round(delta * x, 5)
-        dL_db = round(delta, 5)
+        # dL / dy_hat
+        dL_dyhat = y_hat - y_true
 
-        return (dL_dw, dL_db)
-        
+        # dy_hat / dz
+        dyhat_dz = y_hat * (1.0 - y_hat)
+
+        # dL / dz via chain rule
+        dL_dz = dL_dyhat * dyhat_dz
+
+        # dz / dw = x
+        dL_dw = dL_dz * x
+
+        # dz / db = 1
+        dL_db = dL_dz
+
+        return np.round(dL_dw, 5), round(dL_db, 5)
 
 
